d261f768dd20e8b38beae2739ac3b1b2671a544b
Thea Go <48401959+thaeta@users.noreply.github.com>
2020-03-16 16:13:25 +0800

    removed extra spaces

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,36 +1,35 @@
 <template>
     <nav>
       <v-app-bar flat light dense>
-        <v-btn text color="darkgrey" class="px-2">
-          <v-icon small class="pr-2" color="darkgrey">mdi-handshake</v-icon>
-            <v-toolbar-title class="pr-3 darkgrey--text">
-              <span class="font-weight-light">LS</span>
-              <span class="font-weight-medium">DR</span>
-            </v-toolbar-title>
-        </v-btn>
-        <v-btn text color="darkgrey" class="px-2">
-          <v-icon small class="pr-2 caption">mdi-view-dashboard</v-icon>
-          <span class="font-weight-light">Dashboard</span>
-        </v-btn>
-        <v-btn text color="darkgrey" class="px-2">
-          <v-icon small class="pr-2">mdi-school</v-icon>
-          <span class="font-weight-light">Schools</span>
-        </v-btn>
-        <v-btn text color="darkgrey" class="px-2">
-          <v-icon small class="pr-2">mdi-newspaper-variant-multiple</v-icon>
-          <span class="font-weight-light">Bulletin</span>
-        </v-btn>
-        <v-btn text color="darkgrey" class="px-2">
-          <v-icon small class="pr-2">mdi-comment-question</v-icon>
-          <span class="font-weight-light">About</span>
-        </v-btn>
-      </v-app-bar>
-    </nav>
-    </template>
+      <v-btn text color="darkgrey" class="px-2">
+        <v-icon small class="pr-2" color="darkgrey">mdi-handshake</v-icon>
+          <v-toolbar-title class="pr-3 darkgrey--text">
+            <span class="font-weight-light">LS</span>
+            <span class="font-weight-medium">DR</span>
+          </v-toolbar-title>
+      </v-btn>
+      <v-btn text color="darkgrey" class="px-2">
+        <v-icon small class="pr-2 caption">mdi-view-dashboard</v-icon>
+        <span class="font-weight-light">Dashboard</span>
+      </v-btn>
+      <v-btn text color="darkgrey" class="px-2">
+        <v-icon small class="pr-2">mdi-school</v-icon>
+        <span class="font-weight-light">Schools</span>
+      </v-btn>
+      <v-btn text color="darkgrey" class="px-2">
+        <v-icon small class="pr-2">mdi-newspaper-variant-multiple</v-icon>
+        <span class="font-weight-light">Bulletin</span>
+      </v-btn>
+      <v-btn text color="darkgrey" class="px-2">
+        <v-icon small class="pr-2">mdi-comment-question</v-icon>
+        <span class="font-weight-light">About</span>
+      </v-btn>
+    </v-app-bar>
+  </nav>
+</template>
     
-    <script>
-    export default {
-        data() {
-        }
-    }
-    </script>
+<script>
+export default {
+    data() {}
+}
+</script>
