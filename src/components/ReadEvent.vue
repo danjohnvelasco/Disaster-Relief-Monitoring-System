e44d7f37d8b5df42961ccb0f83e1d45f1bd1493f
Jason Evangelista <jase_evangelista@dlsu.edu.ph>
2020-03-16 16:13:03 +0800

    add readEvent component and route

---
+++ src/components/ReadEvent.vue
@@ -0,0 +1,78 @@
+<template>
+
+  <v-card>
+    
+    <div id="gen_info">
+    <v-card-title>{{disaster.title}}</v-card-title>
+    <v-card-subtitle>Lead School: De La Salle University - Manila</v-card-subtitle>
+    <v-card-text>Disaster Type: {{disaster.type}}</v-card-text>
+    <v-card-text>Location: {{disaster.location}}</v-card-text>
+    <v-card-text>{{disaster.description}}</v-card-text>
+    </div>
+
+    <div id="stats">
+    
+    </div>
+    
+    <div id="call_for_donations">
+    
+    </div>
+
+    <div id="photos">
+    
+    </div>
+
+    <div id="graphs">
+    
+    </div>
+
+  </v-card>
+  
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      disaster: {
+        title: 'Leveriza Fire',
+        type: 'Fire',
+        location: 'Leveriza St, Malate, Manila, 1004 Metro Manila',
+        description: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta who moved my cheese babybel boursin.',
+        fam_affected: null,
+        indiv_affected: null,
+        remarks: null,
+        evac_fam_inside: null,
+        evac_indiv_inside: null,
+        damage_cost: null,
+        structures_damaged: null,
+        donateOption: null,
+        donation_details: null,
+        linkProfile: true,
+        reliefs: []
+      }
+      // disaster: {
+      //   title: null,
+      //   type: null,
+      //   location: null,
+      //   description: null,
+      //   fam_affected: null,
+      //   indiv_affected: null,
+      //   remarks: null,
+      //   evac_fam_inside: null,
+      //   evac_indiv_inside: null,
+      //   damage_cost: null,
+      //   structures_damaged: null,
+      //   donateOption: null,
+      //   donation_details: null,
+      //   linkProfile: true,
+      //   reliefs: []
+      // }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../components/EventForm.vue')
-  }
+  },
+  {
+    path: '/disaster-event',
+    name: 'Disaster Event',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../components/ReadEvent.vue')
+  },
 ]
 
 const router = new VueRouter({
