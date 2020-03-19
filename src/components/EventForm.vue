8b535394057c22ecd5bd5b23d35c421e0447d09e
danjohnvelasco <54620209+danjohnvelasco@users.noreply.github.com>
2020-03-19 14:36:46 +0800

    reused form for (U)pdate module, add event is now overlay, minor navbar changes, removed /event-form route

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -2,207 +2,220 @@
     <v-card class="form">
       <v-card-title>Event Details</v-card-title>
       <v-card-text>
-        <v-form
-          ref="form"
-        >
-        <!--Required data fields starts here-->
-        <v-text-field
-          outlined
-          v-model="disaster.title"
-          :rules="required"
-          label="Title"
-        ></v-text-field>
-        <v-text-field
-          outlined
-          v-model="disaster.type"
-          :rules="required_lettersOnly"
-          label="Disaster Type"
-        ></v-text-field>
-        <v-text-field
-          outlined
-          v-model="disaster.location"
-          :rules="required"
-          label="Location"
-        ></v-text-field>
-        <v-textarea
-          outlined
-          v-model="disaster.description"
-          auto-grow
-          rows="1"
-          :rules="required"
-          label="Description"
-        ></v-textarea>
-        <v-text-field
-          outlined
-          v-model="disaster.fam_affected"
-          :rules="required_numbersOnly"
-          label="Families Affected"  
-          suffix="families affected"            
-        ></v-text-field>
-        <v-text-field
-          outlined
-          v-model="disaster.indiv_affected"
-          :rules="required_numbersOnly"
-          label="Individuals Affected" 
-          suffix="individuals affected"             
-        ></v-text-field>
-        <v-textarea
-          outlined
-          v-model="disaster.remarks"
-          auto-grow
-          rows="4"
-          label="Remarks (optional)"
-        ></v-textarea>
-        <!--Optional data fields starts here-->
-        <v-subheader>Optional Data</v-subheader>
-        <v-expansion-panels>
-          <v-expansion-panel> <!--Group 1-->
-            <v-expansion-panel-header>Evacuation Center Data</v-expansion-panel-header>
-            <v-expansion-panel-content>
-              <v-text-field
-                outlined
-                v-model="disaster.evac_fam_inside"
-                :rules="numbersOnly"
-                label="Families Inside Evacuation Center"          
-                suffix="families inside"    
-              ></v-text-field>
-              <v-text-field
-                outlined
-                v-model="disaster.evac_indiv_inside"
-                :rules="numbersOnly"
-                label="Individuals Inside Evacuation Center"
-                suffix="individuals inside"              
-              ></v-text-field>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-          <v-expansion-panel> <!--Group 2-->
-            <v-expansion-panel-header>Damage Level Data</v-expansion-panel-header>
-            <v-expansion-panel-content>
-              <v-text-field
-                outlined
-                v-model="disaster.damage_cost"
-                :rules="currency"
-                hint="ex. 330000 or 330,000 or 330,000.15"
-                persistent-hint
-                label="Monetary Value (damage cost)"  
-                prefix="₱"            
-              ></v-text-field>
-              <v-text-field
-                outlined
-                v-model="disaster.structures_damaged"
-                :rules="numbersOnly"
-                label="Structures damaged"
-                suffix="structures damaged"              
-              ></v-text-field>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-          <v-expansion-panel> <!--Group 3-->
-            <v-expansion-panel-header>Call for Donations</v-expansion-panel-header>
-            <v-expansion-panel-content>
-              <v-radio-group v-model="disaster.donate_option" row>
-                <v-radio label="In-kind" value="in-kind"></v-radio>
-                <v-radio label="Cash" value="cash"></v-radio>
-                <v-radio label="Both" value="both"></v-radio>
-              </v-radio-group>
-              <div v-if="disaster.donate_option === 'in-kind' || disaster.donate_option === 'both'">
-                <v-subheader>In-kind Donations</v-subheader>
-                <v-row dense v-for="(relief, index) in disaster.reliefs" :key="index">
-                  <v-col cols="4">
-                    <v-text-field
-                      outlined
-                      v-model=disaster.reliefs[index].item          
-                      label="Item"  
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="7">
-                    <v-text-field
-                      outlined
-                      v-model=disaster.reliefs[index].spec            
-                      label="Specifications" 
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="1" :align-self="'start'">
-                    <div class="listButton">
-                      <v-icon x-large @click="deleteItem(index)">mdi-delete</v-icon>
-                    </div>
-                  </v-col>
-                </v-row>
-                <v-row dense>
-                  <v-col cols="4">
-                    <v-text-field
-                      outlined
-                      v-model="item"  
-                      hint="ex. water, canned goods, slippers"   
-                      persistent-hint 
-                      label="Item"    
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="7">
-                    <v-text-field
-                      outlined
-                      v-model="spec"             
-                      hint="ex. 5 liters, atleast 3 months expiration"
-                      persistent-hint
-                      label="Specifications (Optional)"  
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="1" :align-self="'start'">
-                    <div class="listButton">
-                      <v-icon x-large @click="addItem">mdi-plus-circle</v-icon>
-                    </div>
-                  </v-col>
-                </v-row>
-                <v-row dense>
-                  <v-col cols="11"> 
-                    <v-alert
-                      outlined
-                      dense
-                      dismissible
-                      type="info"
-                      text
-                    >
-                      You must press the '+' button to add an item.
-                    </v-alert>
-                  </v-col>
-                </v-row>
-                <v-row dense>
-                  <v-col cols="11">
-                    <v-alert
-                      outlined
-                      dense
-                      type="error"
-                      v-if="add_item_feedback"
-                    >
-                      Please input item name.
-                    </v-alert>
-                  </v-col>
-                </v-row>
-              </div>
-              <div v-if="disaster.donate_option === 'cash' || disaster.donate_option === 'both'">
-                <v-subheader>Cash Donations</v-subheader>
-                <v-textarea
+        <v-form ref="form">
+          <!--Required data fields starts here-->
+          <v-text-field
+            outlined
+            v-model="disaster.title"
+            :rules="required"
+            label="Title"
+          ></v-text-field>
+          <v-text-field
+            outlined
+            v-model="disaster.type"
+            :rules="required_lettersOnly"
+            label="Disaster Type"
+          ></v-text-field>
+          <v-text-field
+            outlined
+            v-model="disaster.location"
+            :rules="required"
+            label="Location"
+          ></v-text-field>
+          <v-textarea
+            outlined
+            v-model="disaster.description"
+            auto-grow
+            rows="1"
+            :rules="required"
+            label="Description"
+          ></v-textarea>
+          <v-text-field
+            outlined
+            v-model="disaster.fam_affected"
+            :rules="required_numbersOnly"
+            label="Families Affected"  
+            suffix="families affected"            
+          ></v-text-field>
+          <v-text-field
+            outlined
+            v-model="disaster.indiv_affected"
+            :rules="required_numbersOnly"
+            label="Individuals Affected" 
+            suffix="individuals affected"             
+          ></v-text-field>
+          <v-textarea
+            outlined
+            v-model="disaster.remarks"
+            auto-grow
+            rows="4"
+            label="Remarks (optional)"
+          ></v-textarea>
+          <!--Optional data fields starts here-->
+          <v-subheader>Optional Data</v-subheader>
+          <v-expansion-panels>
+            <v-expansion-panel> <!--Group 1-->
+              <v-expansion-panel-header>Evacuation Center Data</v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-text-field
                   outlined
-                  v-model="disaster.donation_details"
-                  auto-grow
-                  rows="4"
-                  label="Donation Details"
-                ></v-textarea>
-                <v-subheader>OR</v-subheader>
-                <v-switch v-model="disaster.link_profile" :label="'Link your profile'"></v-switch>
-              </div>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
-        <v-btn
-            :disabled="!valid"
-            color="success"
-            class="mt-10"
-            @click="create"
-        >
-            Submit
-        </v-btn>
+                  v-model="disaster.evac_fam_inside"
+                  :rules="numbersOnly"
+                  label="Families Inside Evacuation Center"          
+                  suffix="families inside"    
+                ></v-text-field>
+                <v-text-field
+                  outlined
+                  v-model="disaster.evac_indiv_inside"
+                  :rules="numbersOnly"
+                  label="Individuals Inside Evacuation Center"
+                  suffix="individuals inside"              
+                ></v-text-field>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+            <v-expansion-panel> <!--Group 2-->
+              <v-expansion-panel-header>Damage Level Data</v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-text-field
+                  outlined
+                  v-model="disaster.damage_cost"
+                  :rules="currency"
+                  hint="ex. 330000 or 330,000 or 330,000.15"
+                  persistent-hint
+                  label="Monetary Value (damage cost)"  
+                  prefix="₱"            
+                ></v-text-field>
+                <v-text-field
+                  outlined
+                  v-model="disaster.structures_damaged"
+                  :rules="numbersOnly"
+                  label="Structures damaged"
+                  suffix="structures damaged"              
+                ></v-text-field>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+            <v-expansion-panel> <!--Group 3-->
+              <v-expansion-panel-header>Call for Donations</v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-radio-group v-model="disaster.donate_option" row>
+                  <v-radio label="In-kind" value="in-kind"></v-radio>
+                  <v-radio label="Cash" value="cash"></v-radio>
+                  <v-radio label="Both" value="both"></v-radio>
+                </v-radio-group>
+                <div v-if="disaster.donate_option === 'in-kind' || disaster.donate_option === 'both'">
+                  <v-subheader>In-kind Donations</v-subheader>
+                  <v-row dense v-for="(relief, index) in disaster.reliefs" :key="index">
+                    <v-col cols="4">
+                      <v-text-field
+                        outlined
+                        v-model=disaster.reliefs[index].item          
+                        label="Item"  
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="7">
+                      <v-text-field
+                        outlined
+                        v-model=disaster.reliefs[index].spec            
+                        label="Specifications" 
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="1" :align-self="'start'">
+                      <div class="listButton">
+                        <v-icon x-large @click="deleteItem(index)">mdi-delete</v-icon>
+                      </div>
+                    </v-col>
+                  </v-row>
+                  <v-row dense>
+                    <v-col cols="4">
+                      <v-text-field
+                        outlined
+                        v-model="item"  
+                        hint="ex. water, canned goods, slippers"   
+                        persistent-hint 
+                        label="Item"    
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="7">
+                      <v-text-field
+                        outlined
+                        v-model="spec"             
+                        hint="ex. 5 liters, atleast 3 months expiration"
+                        persistent-hint
+                        label="Specifications (Optional)"  
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="1" :align-self="'start'">
+                      <div class="listButton">
+                        <v-icon x-large @click="addItem">mdi-plus-circle</v-icon>
+                      </div>
+                    </v-col>
+                  </v-row>
+                  <v-row dense>
+                    <v-col cols="11"> 
+                      <v-alert
+                        outlined
+                        dense
+                        dismissible
+                        type="info"
+                        text
+                      >
+                        You must press the '+' button to add an item.
+                      </v-alert>
+                    </v-col>
+                  </v-row>
+                  <v-row dense>
+                    <v-col cols="11">
+                      <v-alert
+                        outlined
+                        dense
+                        type="error"
+                        v-if="add_item_feedback"
+                      >
+                        Please input item name.
+                      </v-alert>
+                    </v-col>
+                  </v-row>
+                </div>
+                <div v-if="disaster.donate_option === 'cash' || disaster.donate_option === 'both'">
+                  <v-subheader>Cash Donations</v-subheader>
+                  <v-textarea
+                    outlined
+                    v-model="disaster.donation_details"
+                    auto-grow
+                    rows="4"
+                    label="Donation Details"
+                  ></v-textarea>
+                  <v-subheader>OR</v-subheader>
+                  <v-switch v-model="disaster.link_profile" :label="'Link your profile'"></v-switch>
+                </div>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
         </v-form>
       </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn 
+          v-if="editing === false"
+          color="success" 
+          @click="create"
+        > 
+          Submit 
+        </v-btn>
+        <v-btn 
+          v-if="editing === true"
+          color="primary" 
+          @click="create"
+        > 
+          Update
+        </v-btn>
+        <!-- Emits an event back to parent component -->
+        <v-btn 
+          @click="closeForm"
+        > 
+          Close 
+        </v-btn>
+      </v-card-actions>
     </v-card>
 </template>
 
@@ -211,8 +224,19 @@
   import firebase from 'firebase/app'
 
   export default {
+    props: { // can be changed by parent component
+      populateWith: {
+        type: Object,
+        default: () => {
+          return {};
+        }
+      },
+      editing: { 
+        type: Boolean,
+        default: false
+      }
+    },
     data: () => ({
-      editing: false,
       valid: true,
       required_lettersOnly: [
         v => !!v || 'This field is required',
@@ -283,6 +307,9 @@
           });
         }
       },
+      update() {
+
+      },
       addItem() {
         if (this.item) {
           this.disaster.reliefs.push({item: this.item, spec: this.spec});
@@ -302,15 +329,30 @@
         } else {
           console.log('Error deleting item.');
         }
+      },
+      closeForm() { // emits close event to parent component
+        this.$emit('close');
       }
     },
+    created() { // receives the data (if it exists) from parent component --only applies to editing
+      console.log('created');
+      if (Object.keys(this.populateWith).length !== 0) {
+        this.disaster = this.populateWith;
+      } else {
+        console.log('empty');
+      }
+    },
+    beforeDestroy() {
+      console.log('beforeDestroy');
+    }
   }
 </script>
 
 <style>
+/*
     .form {
       margin: 2em 8em;
-    }
+    }*/
 
     .listButton {
       padding: 10px 0px;
--- src/router/index.js
+++ src/router/index.js
@@ -17,14 +17,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/event-form',
-    name: 'Event Form',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../components/EventForm.vue')
   }
 ]
 
