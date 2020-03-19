a116cc72dc010463cb6b244abea31e925039ae22
Kimi Catahan <61366947+annakumiko@users.noreply.github.com>
2020-03-20 02:30:24 +0800

    Added school details page

---
+++ src/components/SchoolDetails.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container id="left">
+    <h1 id="schoolname"> {{school.name}} </h1>
+  </v-container>
+
+
+</template>
+
+<script>
+  export default {
+  data() {
+    return {
+      schools: [
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
