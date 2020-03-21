36558d2c28b26d8dcb56f5c79cc031a6f3b821bc
Kimi Catahan <61366947+annakumiko@users.noreply.github.com>
2020-03-22 02:15:32 +0800

    Update SchoolDetails.vue

--- src/components/SchoolDetails.vue
+++ src/components/SchoolDetails.vue
@@ -4,21 +4,21 @@
       <v-col class="col-md-3 col-lg-3 mb-3">
         <v-container id="short_intro">
           <v-layout column align-center>
-          <v-img :src="school.img" height="270px" width="270px"> </v-img>
+          <v-img :src="schools.img" height="270px" width="270px"> </v-img>
           </v-layout>
           <h1> Contact Us </h1>
-          <h3> {{school.contactnum}} </h3>
-          <h3> {{school.contactperson}} </h3>
-          <h4> {{school.email}} </h4>
+          <h3>{{school.contactnum}}</h3>
+          <h3>{{school.contactperson}}</h3>
+          <h4>{{school.email}}</h4>
           <br> <br>
-          <h3> {{school.webpage}} </h3>
+          <h3>{{school.webpage}} </h3>
           <br>
-          <h5> {{school.address}} </h5>
+          <h5>{{school.address}} </h5>
         </v-container>
       </v-col>
       <v-col class="col-md-8 col-lg-8 mb-8">
         <v-container id="info">
-          <h1 id="schoolname"> {{school.name}} </h1>
+          <h1 id="schoolname">{{school.name}}</h1>
           <v-divider></v-divider>
           <br> <br>
           <p>
@@ -36,14 +36,15 @@
   export default {
     data(){
       return{
-        schools: {},
+        school: {},
         id: 'DLSU'
     }
   },
   
-  methods:{
-
-  },
+    created(){
+    db.collection('schools')
+      .doc(this.id)
+  }
 }
 
 </script>
