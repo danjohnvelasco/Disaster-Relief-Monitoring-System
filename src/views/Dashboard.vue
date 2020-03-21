c6b0f1003f43267e211beb114623db903dcf9f72
danjohnvelasco <54620209+danjohnvelasco@users.noreply.github.com>
2020-03-21 22:01:50 +0800

    render disaster list from firebase data

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,11 +2,11 @@
   <v-app>
     <v-row>
       <v-col class="col-md-3 col-lg-3 mb-3">
-        <DisasterEvents/>
+        <DisasterEvents @displayEvent="displayEvent"></DisasterEvents>
       </v-col>
       <v-col class="col-md-9 col-lg-9 mb-9">
         <v-card dark height="90%" width="100%">
-          <ReadEvent/>
+          <ReadEvent :doc_id="doc_id"></ReadEvent>
         </v-card>
       </v-col>
     </v-row>
@@ -22,6 +22,17 @@ export default {
   components: {
     DisasterEvents,
     ReadEvent
+  },
+  methods: {
+    displayEvent(payload) { // payload is a firebase doc_id from DisasterEvents
+      this.doc_id = payload;
+      console.log(payload);
+    }
+  },
+  data() {
+    return {
+      doc_id: null
+    }
   }
 }
 </script>
