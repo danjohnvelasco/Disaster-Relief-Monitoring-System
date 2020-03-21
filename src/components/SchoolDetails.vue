0397b8e347315023d812f0c14c4ffdfa4d6af6c0
Kimi Catahan <61366947+annakumiko@users.noreply.github.com>
2020-03-21 23:00:43 +0800

    Update SchoolDetails.vue

--- src/components/SchoolDetails.vue
+++ src/components/SchoolDetails.vue
@@ -1,9 +1,31 @@
 <template>
-  <v-container id="left">
-    <h1 id="schoolname"> {{school.name}} </h1>
-  </v-container>
-
-
+  <div class="details">
+    <v-row>
+      <v-col class="col-md-3 col-lg-3 mb-3">
+        <v-container id="short_intro">
+          <v-img src='https://images.squarespace-cdn.com/content/v1/5d09ea045609c00001987da2/1568341161488-G8SYQ6PG4WI0UCJ1T17W/ke17ZwdGBToddI8pDm48kNiEM88mrzHRsd1mQ3bxVct7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0topjEaZcWjtmMYdCWL4dkGbxs35J-ZjFa9s1e3LsxrX8g4qcOj2k2AL08mW_Htcgg/DLSASMC.png?format=300w' height="300px" width="300px"> </v-img>
+          <h1> Contact Us </h1>
+          <h3> Contact Number </h3>
+          <h3> Contact Person </h3>
+          <h4> Email </h4>
+          <br> <br>
+          <h3> Webpage </h3>
+          <br>
+          <h5> Address </h5>
+        </v-container>
+      </v-col>
+      <v-col class="col-md-8 col-lg-8 mb-8">
+        <v-container id="info">
+          <h1 id="schoolname"> {{school.name}} </h1>
+          <v-divider></v-divider>
+          <br> <br>
+          <p>
+            yes bro
+          </p>
+        </v-container>
+      </v-col>  
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -16,5 +38,11 @@
 </script>
 
 <style scoped>
+h1{
+  text-align: center;
+}
 
+#schoolname{
+  text-align: left;
+}
 </style>
