3e40ca84a5ed4bbcdec011f878616a381464e406
danjohnvelasco <54620209+danjohnvelasco@users.noreply.github.com>
2020-04-14 16:38:26 +0800

    adjusted whitespaces, added comments for readability

--- src/components/EventViewer.vue
+++ src/components/EventViewer.vue
@@ -31,68 +31,57 @@
           </table>
         </v-card>
       </v-container>
-
+      <!-- statistics section -->
       <v-container id="stats" class="pb-5">
-        
-          <h2 class="headline text-center">Statistics Overview</h2>
-
-          <v-row wrap class="mx-8">
-
-            <v-col class="col-md-6 col-lg-6 mb-6" align="center">
-              <v-card class="text-center" width='280'>
-                <v-icon size='70' class='mt-3'>mdi-account</v-icon>
-                <v-card-text class="display-2 font-weight-bold">{{disaster.indiv_affected}}</v-card-text>
-                <v-card-text class="title pt-0">Individuals Affected</v-card-text>
-              </v-card>
-            </v-col>
-
-            <v-col class="col-md-6 col-lg-6 mb-6" align="center">
-              <v-card class="text-center" width="280">
-                <v-icon size='70' class='mt-3'>mdi-account-group</v-icon>
-                <v-card-text class="display-2 font-weight-bold">{{disaster.fam_affected}}</v-card-text>
-                <v-card-text class="title pt-0">Families Affected</v-card-text>
-              </v-card>
-            </v-col>
-
-            <v-col  class="col-md-6 col-lg-6 mb-6" align="center" v-if="disaster.evac_indiv_inside != null">
-              <v-card class="text-center" width="280">
-                <v-icon size='70' class='mt-3'>mdi-home-variant</v-icon>
-                <v-card-text class="display-2 font-weight-bold">{{disaster.evac_indiv_inside}}</v-card-text>
-                <v-card-text class="title pt-0">Individuals in Evac Centers</v-card-text>
-              </v-card>
-            </v-col>
-
-            <v-col class="col-md-6 col-lg-6 mb-6" align="center" v-if="disaster.evac_fam_inside != null">
-              <v-card class="text-center" width="280">
-                <v-icon size='70' class='mt-3'>mdi-home-group</v-icon>
-                <v-card-text class="display-2 font-weight-bold">{{disaster.evac_fam_inside}}</v-card-text>
-                <v-card-text class="title pt-0">Families In Evac Centers</v-card-text>
-              </v-card>
-            </v-col>
-
-            <v-col class="col-md-6 col-lg-6 mb-6" align="center" v-if="disaster.damage_cost != null">
-              <v-card class="text-center" width="280">
-                <v-icon size='70' class='mt-3'>mdi-cash</v-icon>
-                <v-card-text class="display-2 font-weight-bold">{{disaster.damage_cost}}</v-card-text>
-                <v-card-text class="title pt-0">Damage Cost (PHP)</v-card-text>
-              </v-card>
-            </v-col>
-
-            <v-col class="col-md-6 col-lg-6 mb-6" align="center" v-if="disaster.structures_damaged != null">
-              <v-card class="text-center" width="280">
-                <v-icon size='70' class='mt-3'>mdi-domain</v-icon>
-                <v-card-text class="display-2 font-weight-bold">{{disaster.structures_damaged}}</v-card-text>
-                <v-card-text class="title pt-0">Structures Damaged</v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-
-        
+        <h2 class="headline text-center">Statistics Overview</h2>
+        <v-row wrap class="mx-8">
+          <v-col class="col-md-6 col-lg-6 mb-6" align="center">
+            <v-card class="text-center" width='280'>
+              <v-icon size='70' class='mt-3'>mdi-account</v-icon>
+              <v-card-text class="display-2 font-weight-bold">{{disaster.indiv_affected}}</v-card-text>
+              <v-card-text class="title pt-0">Individuals Affected</v-card-text>
+            </v-card>
+          </v-col>
+          <v-col class="col-md-6 col-lg-6 mb-6" align="center">
+            <v-card class="text-center" width="280">
+              <v-icon size='70' class='mt-3'>mdi-account-group</v-icon>
+              <v-card-text class="display-2 font-weight-bold">{{disaster.fam_affected}}</v-card-text>
+              <v-card-text class="title pt-0">Families Affected</v-card-text>
+            </v-card>
+          </v-col>
+          <v-col  class="col-md-6 col-lg-6 mb-6" align="center" v-if="disaster.evac_indiv_inside != null">
+            <v-card class="text-center" width="280">
+              <v-icon size='70' class='mt-3'>mdi-home-variant</v-icon>
+              <v-card-text class="display-2 font-weight-bold">{{disaster.evac_indiv_inside}}</v-card-text>
+              <v-card-text class="title pt-0">Individuals in Evac Centers</v-card-text>
+            </v-card>
+          </v-col>
+          <v-col class="col-md-6 col-lg-6 mb-6" align="center" v-if="disaster.evac_fam_inside != null">
+            <v-card class="text-center" width="280">
+              <v-icon size='70' class='mt-3'>mdi-home-group</v-icon>
+              <v-card-text class="display-2 font-weight-bold">{{disaster.evac_fam_inside}}</v-card-text>
+              <v-card-text class="title pt-0">Families In Evac Centers</v-card-text>
+            </v-card>
+          </v-col>
+          <v-col class="col-md-6 col-lg-6 mb-6" align="center" v-if="disaster.damage_cost != null">
+            <v-card class="text-center" width="280">
+              <v-icon size='70' class='mt-3'>mdi-cash</v-icon>
+              <v-card-text class="display-2 font-weight-bold">{{disaster.damage_cost}}</v-card-text>
+              <v-card-text class="title pt-0">Damage Cost (PHP)</v-card-text>
+            </v-card>
+          </v-col>
+          <v-col class="col-md-6 col-lg-6 mb-6" align="center" v-if="disaster.structures_damaged != null">
+            <v-card class="text-center" width="280">
+              <v-icon size='70' class='mt-3'>mdi-domain</v-icon>
+              <v-card-text class="display-2 font-weight-bold">{{disaster.structures_damaged}}</v-card-text>
+              <v-card-text class="title pt-0">Structures Damaged</v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-container>
-
+      <!-- call for donation section -->
       <v-container id="call_for_donations" v-if="disaster.donate_option != null">
         <h2 class="headline text-center">Call for Donations</h2>
-
         <v-row wrap class="">
           <v-col cols="12" align="center" v-if="disaster.donate_option == 'cash' || disaster.donate_option == 'both'">
             <v-card width="500">
@@ -107,11 +96,9 @@
             </v-card>
           </v-col>
         </v-row>
-
       </v-container>
-
+      <!-- gallery section -->
       <v-container id="gallery" v-if="file_URLs != undefined && file_URLs.length > 0">
-
         <h2 class="headline text-center">Gallery</h2>
           <v-container class='pl-5 pr-5 pb-5 text-center'>
             <v-carousel cycle height="auto" style="width: 700px; margin: auto;" hide-delimiter-background show-arrows-on-hover>
@@ -119,11 +106,9 @@
               </v-carousel-item>
             </v-carousel>
           </v-container>
-      
       </v-container>
-
+      <!-- historical data section -->
       <v-container id="historical_data" >
-
         <h2 class="headline text-center">Historical Data</h2>
         <v-card style="width: 800px; margin: auto;" class="mt-4">
           <v-simple-table class="">
@@ -143,21 +128,20 @@
             </tbody>
           </v-simple-table>
         </v-card>
-        
       </v-container>
-    
     </v-content>
   </v-app>
 </template>
 
 <script>
-import {db, storage } from '@/firebase/init'
+import { db, storage } from '@/firebase/init'
 import EventForm from '@/components/EventForm'
 
 export default {
   components: {
     EventForm
   },
+  props: ['doc_id'],
   data(){
     return{
       disaster: {},
@@ -167,7 +151,6 @@ export default {
       editing: false,
     }
   },
-  props: ['doc_id'],
   watch: {
     doc_id(newVal) {
       this.getData(newVal);
@@ -188,21 +171,21 @@ export default {
       var newdate = (date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear();
       return newdate
     },
-    getImageURLs: function (doc_id, files){
+    getImageURLs: function (doc_id, files) {
       // create storage reference
       var storageRef = storage.ref()
       // create child reference, which points to 'disaster_id' folder
       var imageRef = storageRef.child(doc_id)
       // pushes each image download URL to file_URLs array
       files.forEach((file) => {
-        imageRef.child(file).getDownloadURL().then((url) =>{
+        imageRef.child(file).getDownloadURL().then((url) => {
           this.file_URLs.push(url)
-        }).catch(err =>{
+        }).catch(err => {
           console.log('Image Error: ' + err)
         })
       })
     },
-    getHistoricalData: function(list){
+    getHistoricalData: function(list) {
       var historical_data = {
         date: '',
         indiv_affected: '',
@@ -219,34 +202,32 @@ export default {
     getData: function (doc_id) {
       db.collection('disasters2')
       .doc(doc_id)
-      .collection('history').orderBy('created_at').get().then(doc =>{
-          if (doc){
-            // clears current data in disaster form
-            this.clearData()
-            // rearranges disaster historical data array into latest first
-            var disaster_data = doc.docs.reverse()
-            // grabs latest data in history subcollection
-            this.disaster = disaster_data[0].data()
-            // converts timestamp data type into Date
-            this.disaster.created_at = this.timestampToDate(this.disaster.created_at)
-            // converts image URLs to download URLs and transfers to file_URLs array
-            if(this.disaster.img_URLs != undefined && this.disaster.img_URLs.length > 0)
-              this.getImageURLs(doc_id, this.disaster.img_URLs)
-            // gets historical data of disaster event
-            this.getHistoricalData(disaster_data)
-          } else{
-          console.log('no doc found')
-          }
-        }).catch(err =>{
-          console.log("Error: " + err)
+      .collection('history').orderBy('created_at').get().then(doc => {
+        if (doc) {
+          // clears current data in disaster form
+          this.clearData()
+          // rearranges disaster historical data array into latest first
+          var disaster_data = doc.docs.reverse()
+          // grabs latest data in history subcollection
+          this.disaster = disaster_data[0].data()
+          // converts timestamp data type into Date
+          this.disaster.created_at = this.timestampToDate(this.disaster.created_at)
+          // converts image URLs to download URLs and transfers to file_URLs array
+          if(this.disaster.img_URLs != undefined && this.disaster.img_URLs.length > 0)
+            this.getImageURLs(doc_id, this.disaster.img_URLs)
+          // gets historical data of disaster event
+          this.getHistoricalData(disaster_data)
+        } else{
+        console.log('no doc found')
+        }
+      }).catch(err => {
+        console.log("Error: " + err)
       })
     }
   },
-  
   created() {
     this.getData(this.doc_id);
   }
- 
 }
 </script>
 
