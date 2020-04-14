0d0e38de617de92412d321c45eda790caf9267d0
danjohnvelasco <54620209+danjohnvelasco@users.noreply.github.com>
2020-04-14 16:20:08 +0800

    changed css to scoped, moved some ccs to top-level

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-  <template>
+<template>
   <v-app>
     <Navbar/>
     <v-content>
@@ -17,3 +17,18 @@ export default {
   }
 };
 </script>
+
+<style>
+::-webkit-scrollbar {
+  width: 5px;
+}
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+::-webkit-scrollbar-thumb {
+  background: rgb(165, 165, 165); 
+}
+::-webkit-scrollbar-thumb:hover {
+  background: rgb(100, 100, 100); 
+}
+</style>
--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -19,10 +19,10 @@
 import {db} from '@/firebase/init'
 
 export default {
-  data(){
+  data() {
     return{
       disasters: []
-      }
+    }
   },
   methods: {
     timestampToDate: (timestamp) => {
@@ -58,17 +58,3 @@ export default {
 }
 </script>
 
-<style>
-::-webkit-scrollbar {
-  width: 5px;
-}
-::-webkit-scrollbar-track {
-  background: #f1f1f1; 
-}
-::-webkit-scrollbar-thumb {
-  background: rgb(165, 165, 165); 
-}
-::-webkit-scrollbar-thumb:hover {
-  background: rgb(100, 100, 100); 
-}
-</style>
