afd1e4f1de77621ee52407dbe3946756aaa48e58
Jason Evangelista <jase_evangelista@dlsu.edu.ph>
2020-04-17 19:04:07 +0800

    changed colors to list

--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -1,14 +1,14 @@
 <template>
   <v-app>
     <v-navigation-drawer clipped floating fixed width="25%" class="pt-12" id="body">
-      <h2 class="headline mx-6 mt-8 mb-4" style="color:white;"><b>Disaster Events</b></h2>
+      <h2 class="headline mx-6 mt-8 mb-4" align="center" style="color:#184725;"><b>Disaster Events</b></h2>
       
       <v-card active-class="test" width='auto' class="mb-6 mx-4 d-card" id="card"  v-for="disaster in disasters" :key="disaster.id" @click="displayEvent(disaster.id)">
         <v-list-item three-line>
           <v-list-item-content>
-            <v-list-item-title class="headline mb-1">{{disaster.title}}</v-list-item-title>
-            <v-list-item-subtitle>Lead School: //lead school</v-list-item-subtitle>
-            <div class="overline mt-2" >Updated: {{disaster.last_updated}}</div>
+            <v-list-item-title class="headline mb-1" style="color: white;">{{disaster.title}}</v-list-item-title>
+            <v-list-item-subtitle style="color: white;">Lead School: //lead school</v-list-item-subtitle>
+            <div class="overline mt-2" style="color: white;">Updated: {{disaster.last_updated}}</div>
 
           </v-list-item-content>
         </v-list-item>
@@ -68,16 +68,16 @@ export default {
 }
 
 #body{
-  background-color:#427f50;
+  background-color:;
   padding-right: 10px;
 }
 
 .d-card{
-  background-color:#f7f5e8;
+  background-color:#427f50;
 }
 
 </style>
-
+#beddc6
 #body{
   background-color:#e8e8e8;
 }
